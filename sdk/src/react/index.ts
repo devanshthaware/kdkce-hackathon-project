f01874e8cb8511f2f02bfcab1b6e7b34c0e8786e
devanshthaware <bin.devansh@gmail.com>
2026-04-10 13:18:11 +0530

    fool

--- sdk/src/react/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * React integration for @aegis/auth-sdk
- * Provides context provider and hooks for seamless React integration
- */
-
-export { AegisProvider, useAegisClient } from "./AegisProvider";
-export { useAegisAuth } from "./useAegisAuth";
-
-export type { } from "./useAegisAuth";
